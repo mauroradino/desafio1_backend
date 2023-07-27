a5906a9eba6b0516ee3d918569cff28f0691a35f
mauroradino <mauroradino2003@gmail.com>
2023-07-27 19:55:09 -0300

    arreglos

--- main.js
+++ main.js
@@ -1,45 +1,46 @@
 
 
 class Product {
-    constructor (Nombre, Precio, Descripcion, Stock, Thumbnail, Code){
-        this.Nombre = Nombre;
-        this.Precio = Precio;
-        this.Descripcion = Descripcion;
-        this.Stock = Stock;
-        this.Thumbnail = Thumbnail;
-        this.Code = Code;
+    constructor (nombre, precio, descripcion, stock, thumbnail, code){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.Id = productList.length + 1;
     }
 }
-let productList = [];
 
-const addProduct = (Nombre, Precio, Descripcion, Stock, Thumbnail, Code) => {
-    const isCodeUsed = productList.some(product => product.Code === Code);
+class ProductManager {
+
+static productList = [];
+
+static addProduct = (nombre, precio, descripcion, stock, thumbnail, code, Id) => {
+    const isCodeUsed = productList.some(product => product.code === code);
  if(isCodeUsed){
-    console.log("ese Code ya esta en uso")
+    console.log("ese code ya esta en uso")
     return;
   }
 
-  const nuevoProducto = new Product(Nombre, Precio, Descripcion, Stock, Thumbnail, Code);
+  const nuevoProducto = new Product(nombre, precio, descripcion, stock, thumbnail, code, Id);
+  
   productList.push(nuevoProducto);
 
-    console.log("codigo de producto nuevo", Code) 
+    console.log("codigo de producto nuevo", code) 
     console.log("La lista", productList)
   
  }
 
 
-const getProducts = () => {
+static getProducts = () => {
     console.log(productList)
 }
 
-const getProductById = () =>{
+static getProductById = () =>{
     let id = prompt("Inserte el ID a buscar");
-    productList.map((product) =>{
-    if(product.Code === id){
-        console.log("producto encontrado", product)
-    }
-    else if (product.code != id){
-        console.log("producto no encontrado")
-    }
-    })
+    const encontrado = productList.find((product) => product.Id = id)
+    console.log(encontrado);
+    encontrado === undefined ? console.log("Producto no encontrado") : null
+}
 }
